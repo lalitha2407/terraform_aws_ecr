bc6038e2515b994edcd3864d2bceffd232b15c7c
lalitha2407 <106377943+lalitha2407@users.noreply.github.com>
2022-12-29 14:19:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ resource "aws_ecr_repository" "container_repository" {
   name                 = var.name
   force_delete         = var.force_delete
   image_tag_mutability = var.image_tag_mutability
+  encryption_type      = "AES256"
 
   image_scanning_configuration {
     scan_on_push = true
